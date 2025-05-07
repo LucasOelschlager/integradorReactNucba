1f196efdd7a73d402a9ce1ccf213419e3a554665
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-05-07 20:58:34 -0300

    se añadio el menu de opciones responsive del navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,49 @@
 import { List } from "../List/List";
-import { useContext } from "react";
+import { use, useContext, useState } from "react";
 import styledNavbar from "../Navbar/Navbar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import {
+  faCartShopping,
+  faXmark,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import { UserContext } from "../../context/userContext";
 import { CartComponent } from "../cartComponent/CartComponent";
 import { CartContext } from "../../context/cartContext";
+
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { isLoggedIn, setIsLoggedIn } = useContext(UserContext);
   const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+
   return (
     <header className={styledNavbar.header}>
-      <nav className={styledNavbar.navbar}>
+      <div className={`${styledNavbar.logoContainerRes}`}>
+        <img
+          src="./src/assets/navbar/logoNavbar.png"
+          alt=""
+          className="size-25"
+        />
+        <h1 className={styledNavbar.title}>Rosario Guitar Store</h1>
+      </div>
+      <button
+        className={`${styledNavbar.menuClose}`}
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <FontAwesomeIcon
+          icon={isMenuOpen ? faXmark : faBars}
+          style={{ color: "#ff7d00", fontSize: "30px" }}
+        />
+      </button>
+      <nav
+        className={
+          isMenuOpen === false
+            ? `${styledNavbar.navbar}`
+            : `${styledNavbar.navbarRes}`
+        }
+      >
         <div className={`${styledNavbar.logoContainer}`}>
           <img
             src="./src/assets/navbar/logoNavbar.png"
@@ -22,7 +52,6 @@ export const Navbar = () => {
           />
           <h1 className={styledNavbar.title}>Rosario Guitar Store</h1>
         </div>
-
         <List query={styledNavbar.list}>
           <li className={styledNavbar.navbar_link}>
             <Link to="/">Inicio</Link>
@@ -50,15 +79,18 @@ export const Navbar = () => {
             <FontAwesomeIcon
               icon={faCartShopping}
               style={{ color: "#ff7d00" }}
-              onClick={() => setIsCartOpen(true)}
+              onClick={() => {
+                setIsMenuOpen(false);
+                setIsCartOpen(!isCartOpen);
+              }}
             />
           </button>
           <button className={`${styledNavbar.loginBtn}`}>
             <Link to={"/login"}>Iniciar Sesión</Link>
           </button>
         </div>
-        {isCartOpen === true ? <CartComponent /> : null}
       </nav>
+      {isCartOpen === true ? <CartComponent /> : null}
     </header>
   );
 };
